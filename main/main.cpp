2d1d266cad75a5c74ac10a6bf36774ac22a4cefa
Shadman Muhtasim Ariq <shadmanmuhtasim2005@gmail.com>
2020-11-02 19:15:01 +0600

    messege

---
+++ main/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string bookTitle = "Game Of Thrones";
+    string bookAuthor = "George R.R Martin";
+    int bookPrice = 904;
+    int bookISBNNumber = 978-0-00-742854-0;
+    
+    cout << "Title: " << bookTitle << endl;
+    cout << "Author: " << bookAuthor << "\nPrice: BDT" << bookPrice << "\nISBN Number: " << bookISBNNumber << endl;
+
+    return 1;
+}
