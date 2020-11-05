bea630e41de21e0a3192dee4823d23f0ce80e0ae
Shadman Muhtasim Ariq <shadmanmuhtasim2005@gmail.com>
2020-11-05 18:46:42 +0600

    Added user input

---
+++ UserInput/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string carModel;
+    int carYear;
+    string carColor;
+    bool is4wheel;
+
+    cout << "Input the Car Model: ";
+    cin >> carModel;
+    
+    cout << "Input the Car Year: ";
+    cin >> carYear;
+    
+    cout << "Input the Car Color: ";
+    cin >> carColor;
+    
+    cout << "is the Car Four wheel drive??: ";
+    cin >> is4wheel;
+
+    cout << "Car Model: " << carModel << endl;
+    cout << "Car Year: " << carYear << endl;
+    cout << "Car Color: " << carColor << endl;
+    cout << "Is The car Four Wheel Drive??" << endl; 
+}
