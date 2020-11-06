19ea9992f14c7630a9311ad5e3b960d3848689bd
Shadman Muhtasim Ariq <shadmanmuhtasim2005@gmail.com>
2020-11-06 17:52:02 +0600

    Calculator with switch case

---
+++ calc-switch-case/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    double value1, value2;
+    // double value2;
+
+    cout << "Enter value 1 = ";
+    cin >> value1;
+
+    cout << "Enter value 2 = ";
+    cin >> value2;
+
+    cout << "Enter operator = ";
+    cin >> op;
+
+    switch (op)
+    {
+        case '+':
+            cout << value1 << " + " << value2 << " = " << value1 + value2;
+            break;
+        
+        case '-':
+            cout << value1 << " - " << value2 << " = " << value1 + value2;
+            break;
+        
+        case '*':
+            cout << value1 << " * " << value2 << " = " << value1 + value2;
+            break;
+        
+        case '/':
+            cout << value1 << " / " << value2 << " = " << value1 + value2;
+            break;
+        
+        default:
+            cout << "There was a proble retriving the data. Try again.";
+            break;
+    }
+
+    return 0;
+}
