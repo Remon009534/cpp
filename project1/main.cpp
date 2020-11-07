306fad729eec763a655063266337f8630d09a0be
Shadman Muhtasim Ariq <shadmanmuhtasim2005@gmail.com>
2020-11-07 18:31:44 +0600

    Added Continuer Feature

--- project1/main.cpp
+++ project1/main.cpp
@@ -6,6 +6,7 @@ int main()
     int value1;
     int value2;
     string z;
+    string con;
 
     cout << "Enter first value: ";
     cin >> value1;
@@ -16,22 +17,22 @@ int main()
     cout << "Enter a operator: ";
     cin >> z;
 
-    if(z == "+")
+    if (z == "+")
     {
         cout << value1 << " + " << value2 << " = " << value1 + value2 << endl;
     }
 
-    else if(z == "-")
+    else if (z == "-")
     {
         cout << value1 << " - " << value2 << " = " << value1 + value2 << endl;
     } 
 
-    else if(z == "*")
+    else if (z == "*")
     {
         cout << value1 << " * " << value2 << " = " << value1 + value2 << endl;
     }
 
-    else if(z == "/")
+    else if (z == "/")
     {
         cout << value1 << " / " << value2 << " = " << value1 + value2 << endl;
     }
@@ -40,6 +41,21 @@ int main()
     {
         cout << "Wrong input" << endl;
     }
+
+    cout << "\nDo you want to continue = ";
+    cin >> con;
+
+    if (con == "yes")
+    {
+        main();
+    }
+
+    else
+    {
+        exit;
+    }
+    
+
 
     return 0;
 }
