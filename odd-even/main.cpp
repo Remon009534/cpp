bb57199bac4f3077aa1dae7de5052b175f1fa303
Shadman Muhtasim Ariq <shadmanmuhtasim2005@gmail.com>
2020-11-19 18:41:30 +0600

    Made some changes So that both works at the sem time

--- odd-even/main.cpp
+++ odd-even/main.cpp
@@ -6,36 +6,68 @@ int main()
 {
 	int number;
 	int limit;
+	int z = 2;
 
+// Wanted to do smh bout the whole number thingy but failed 
+	cout << "Please Enter A Whole Number or The Program Won't Work Properly" << endl;
 	cout << "Enter a number : ";
 	cin >> number;
 
+// Taking input for where to stop the program
 	cout << "Enter the limit : ";
 	cin >> limit;
 
+	// Need this two variables to work with the while
+	int j = number;
 	int i = number;
 
-	if (i / 2)
+// To Check if the number is divisible by 2 or not if it then it is an even number
+	if (z % i == 0)
 	{
-		cout << "\n********* Even Numbers *******\n" << endl;
+		cout << "\n********* Even Numbers *********\n" << endl;
 
-		while(i <= limit)
+		while (i <= limit)
 		{
-			cout << i << "," << " ";
+			cout << i << ", " << " ";
 			i++;
 			i++;
 		}
+
+		cout << endl;
+		cout << "\n********* Odd Numbers *********\n" << endl;
+		
+		j = number + 1;
+
+		while (j <= limit)
+		{
+			cout << j << ", " << " ";
+			j++;
+			j++;
+		}
+
 	}
 
-	else
-	{
-		cout << "\n********* Odd Numbers *******\n" << endl;
-		
-		while(i <= limit)
+	else 
+	{	
+		cout << "\n********* Odd Numbers *********\n" << endl;
+
+		while (i <= limit)
 		{
-			cout << i << "," << " ";
+			cout << i << ", " << " ";
 			i++;
 			i++;
+		}
+
+		cout << endl;
+		cout << "\n********* Even Numbers *********\n" << endl;
+		
+		j = number + 1;
+
+		while (j <= limit)
+		{
+			cout << j << ", " << " ";
+			j++;
+			j++;
 		}
 	}
 
