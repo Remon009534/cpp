abe7684e2d655c2af4643b6864238d0a763625a3
Shadman Muhtasim Ariq <shadmanmuhtasim2005@gmail.com>
2020-11-20 11:07:54 +0600

    Made some corretions

--- project1/main.cpp
+++ project1/main.cpp
@@ -8,6 +8,7 @@ int main()
     string z;
     string con;
 
+    cout <<  "\nPlease Enter The value Correctly to run the prigram perfectly." << endl;
     cout << "Enter first value: ";
     cin >> value1;
 
@@ -24,22 +25,22 @@ int main()
 
     else if (z == "-")
     {
-        cout << value1 << " - " << value2 << " = " << value1 + value2 << endl;
+        cout << value1 << " - " << value2 << " = " << value1 - value2 << endl;
     } 
 
     else if (z == "*")
     {
-        cout << value1 << " * " << value2 << " = " << value1 + value2 << endl;
+        cout << value1 << " * " << value2 << " = " << value1 * value2 << endl;
     }
 
     else if (z == "/")
     {
-        cout << value1 << " / " << value2 << " = " << value1 + value2 << endl;
+        cout << value1 << " / " << value2 << " = " << value1 / value2 << endl;
     }
 
     else
     {
-        cout << "Wrong input" << endl;
+        cout << "\nWrong input" << endl;
     }
 
     cout << "\nDo you want to continue = ";
